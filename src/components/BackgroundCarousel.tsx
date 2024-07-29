a187fe7d0f6baaa90f0b025f5299e0344b8b9fdd
Adam <faiku@proton.me>
2024-07-29 23:53:14 +0200

    initial animation

--- src/components/BackgroundCarousel.tsx
+++ src/components/BackgroundCarousel.tsx
@@ -1,9 +1,29 @@
 
-
+const logos = [
+    'React.webp',
+    'symfony.png',
+    'NestJS.webp',
+    'NodeJS.webp',
+    'expressjs.png',
+    'PHP.webp',
+    'javascript.png',
+    'typescript.png',
+    'sql.png',
+    'postgre.png',
+    'mongo.svg',
+    'git.png',
+];
 
 export default function BackgroundCarousel() {
     return (
-        <>
-        </>
-    )
+        <div className="background-carousel absolute grayscale opacity-5 rotate-[-10deg]">
+            <div className="scroll-container flex flex-row justify-center items-center gap-4">
+                {logos.map((logo, index) => (
+                    <div key={index} className="logo-container">
+                        <img src={`/icons/${logo}`} alt={`Logo${index+1}`} className="w-32 object-cover" />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
